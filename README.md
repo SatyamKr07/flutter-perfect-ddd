9cf97fdec88c90a78736fc7a1c058417ac3c6b63
Satyam Kumar <satyam@LAP-298-XHQL.local>
2024-07-05 15:14:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_perfect_ddd
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_perfect_ddd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_perfect_ddd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
